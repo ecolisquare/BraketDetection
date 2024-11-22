90d9fd27685c07e6c17788d161b18f505fa9a7fe
ecolisquare <1427359791@qq.com>
2024-11-22 10:46:31 +0800

    添加标注程序

--- segmentation.py
+++ segmentation.py
@@ -6,7 +6,8 @@ from infoextraction import *
 import numpy as np
 from plot_geo import *
 from config import *
-
+# from DGCNN_model import *
+from tqdm import tqdm
 segmentation_config=SegmentationConfig()
 
 json_path = input("请输入路径: ")
@@ -14,15 +15,7 @@ if segmentation_config.verbose:
     print("读取json文件")
 #文件中线段元素的读取和根据颜色过滤
 elements,ori_segments=readJson(json_path)
-# braket_texts=findBraketByHints(elements)
-# for e in elements:
-#     if isinstance(e,DText):
-#         texts.append(e)
-#         print(e)
-# print(len(texts))
-# for t in braket_texts:
-#     print(t)
-# print(len(braket_texts))
+text_and_dimensions=findAllTextAndDimensions(elements)
 if segmentation_config.verbose:
     print("json文件读取完毕")
 #将线进行适当扩张
@@ -31,8 +24,15 @@ segments=expandFixedLength(ori_segments,segmentation_config.line_expand_length)
 #找出所有包含角隅孔圆弧的基本环
 polys, new_segments, point_map,star_pos_map,cornor_holes=findClosedPolys_via_BFS(elements,segments,segmentation_config)
 
+# #预训练的几何分类模型筛选肘板
+# model_path = "/home/user4/BraketDetection/DGCNN/cpkt/geometry_classifier.pth"
+# polys = filter_by_pretrained_DGCNN_Model(polys, model_path)
 
-text_and_dimensions=findAllTextAndDimensions(elements)
+# print("DGCNN筛选后剩余回路: ", len(polys))
+# outputPolysAndGeometry(polys,segmentation_config.poly_image_dir,segmentation_config.draw_polys,segmentation_config.draw_geometry,segmentation_config.draw_poly_nums)
+
+
+
 #结构化输出每个肘板信息
 polys_info = []
 pbar=tqdm(total=len(polys),desc="正在输出结构化信息")
