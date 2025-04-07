0868134dfcbe42d97d24994076e750e3d7de10c6
Robot00010086 <1975804693@qq.com>
2025-04-07 17:22:22 +0800

    update

--- segmentation2.py
+++ segmentation2.py
@@ -90,8 +90,6 @@ if __name__ == '__main__':
     edges_infos,poly_centroids,hint_infos,meta_infos=diffusion_step(edges_infos,poly_centroids,hint_infos,meta_infos)
 
     polys_info,classi_res=classificationAndOutputStep(indices,edges_infos,poly_centroids,hint_infos,meta_infos,segmentation_config)
-
-<<<<<<< HEAD
     free_edge_handles = []
     for idx,(poly_refs,cls) in enumerate(zip(polys_info,classi_res)):
         if cls=='Unclassified' or cls=='Unstandard':
@@ -100,21 +98,6 @@ if __name__ == '__main__':
             for seg in poly_refs:
                 if seg.isConstraint == False and seg.isCornerhole == False:
                     free_edge_handles.append(seg.ref.handle)
-    
-=======
-    # 记录自由边句柄
-    free_edge_handles = []
-    for idx, (poly_refs, classification) in enumerate(zip(polys_info, classi_res)):
-        if classi_res == "Unclassified" or classi_res == "Unstandard":
-            continue
-        else:
-            for seg in poly_refs:
-                if seg.isCornerhole == False and seg.isConstraint == False:
-                    if seg.ref.handle not in free_edge_handles:
-                        free_edge_handles.append(seg.ref.handle)
-
-
->>>>>>> cd276b19e17548a07c7ede24bc89cf246cb9e10e
     bboxs = []
     for poly_refs in polys_info:
         max_x = float('-inf')
