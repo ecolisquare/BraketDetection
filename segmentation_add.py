3bd3df58b114d804b171b99d7b835dbcc81c33a9
ecolisquare <1427359791@qq.com>
2025-05-09 02:19:43 -0400

    update seg_add.py

--- segmentation_add.py
+++ segmentation_add.py
@@ -165,6 +165,7 @@ if __name__ == '__main__':
 
     polys_info,classi_res=classificationAndOutputStep(indices,edges_infos,poly_centroids,hint_infos,meta_infos,segmentation_config)
     free_edge_handles = []
+    non_free_edge_handles = []
     for idx,(poly_refs,cls) in enumerate(zip(polys_info,classi_res)):
         if cls=='Unclassified' or cls=='Unstandard':
             continue
@@ -172,6 +173,8 @@ if __name__ == '__main__':
             for seg in poly_refs:
                 if seg.isConstraint == False and seg.isCornerhole == False:
                     free_edge_handles.append(seg.ref.handle)
+                else:
+                    non_free_edge_handles.append(seg.ref.handle)
     bboxs = []
     for poly_refs in polys_info:
         max_x = float('-inf')
@@ -194,4 +197,4 @@ if __name__ == '__main__':
     
     dxf_path = os.path.splitext(segmentation_config.json_path)[0] + '.dxf'
     dxf_output_folder = segmentation_config.dxf_output_folder
-    draw_rectangle_in_dxf(dxf_path, dxf_output_folder, bboxs, classi_res,indices, free_edge_handles)
+    draw_rectangle_in_dxf(dxf_path, dxf_output_folder, bboxs, classi_res,indices, free_edge_handles, non_free_edge_handles)
